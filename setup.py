bd7a9d317e1f5be8729ee748f0d53aa7a014ebce
anuragreddy Naredla <anuragreddy1241@gmail.com>
2023-12-27 12:31:34 +0530

    setup_python file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="Medical Chatbot",
+    version="0.0.0",
+    author="AnuragReddy Naredla",
+    author_email="anuragreddy1241@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
